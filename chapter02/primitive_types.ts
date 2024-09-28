efd0921ba1fc12d22c4f29caaa6c96305a022a62
shakil ahmed <shakiltha338@gmail.com>
2024-09-28 22:05:08 +0600

    add with type guard function

--- chapter02/primitive_types.ts
+++ chapter02/primitive_types.ts
@@ -35,3 +35,23 @@ function printObject(obj: string | number) {
 
 printObject(1);
 printObject("string value");
+
+// type guards
+
+function addWithTypeGuard(arg1: string | number, arg2: string | number) {
+  if (typeof arg1 === "string") {
+    console.log(`arg1 is of type string`);
+    return arg1 + arg2;
+  }
+  if (typeof arg1 === "number" && typeof arg2 === "number") {
+    // both are numbers
+    console.log(`arg1 and arg2 are numbers`);
+    return arg1 + arg2;
+  }
+  console.log(`default return treat both as strings`);
+  return arg1.toString() + arg2.toString();
+}
+
+console.log(`"1", "2" = ${addWithTypeGuard("1", "2")}`);
+console.log(`1, 2 = ${addWithTypeGuard(1, 2)}`);
+console.log(`1, "2" = ${addWithTypeGuard(1, "2")}`);
