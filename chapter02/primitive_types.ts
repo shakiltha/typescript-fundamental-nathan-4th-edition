2dc2f26a0c7639eeb9b57ef672a3a40b3b46c326
shakil ahmed <shakiltha338@gmail.com>
2024-09-29 14:01:29 +0600

    object types

--- chapter02/primitive_types.ts
+++ chapter02/primitive_types.ts
@@ -208,3 +208,18 @@ console.log(`globalString = ${globalString}`);
 function setGlobalString(value: string) {
   globalString = value;
 }
+
+// object
+let structuredObject: object = {
+  name: "myObject",
+  properties: {
+    id: 1,
+    type: "AnObject",
+  },
+};
+
+function printObjectType(a: object) {
+  console.log(`a: ${JSON.stringify(a)}`);
+}
+
+printObjectType(structuredObject);
