f4f177e0531643ad7552b6deee0d6fe5b07f9ff8
shakil ahmed <shakiltha338@gmail.com>
2024-09-30 06:28:19 +0600

    in operator

--- chapter03/interfaces.ts
+++ chapter03/interfaces.ts
@@ -34,3 +34,26 @@ let weakTypeNoOverlap: IWeakType = {
   // description: "a description",
   name: "name",
 };
+
+// in operator
+interface IIdName {
+  id: number;
+  name: string;
+}
+
+interface IDescrValue {
+  descr: string;
+  value: number;
+}
+
+function printNameOrValue(obj: IIdName | IDescrValue): void {
+  if ("id" in obj) {
+    console.log(`obj.name : ${obj.name}`);
+  }
+  if ("descr" in obj) {
+    console.log(`obj.name : ${obj.value}`);
+  }
+}
+
+printNameOrValue({ id: 1, name: "nameValue" });
+printNameOrValue({ descr: "description", value: 2 });
