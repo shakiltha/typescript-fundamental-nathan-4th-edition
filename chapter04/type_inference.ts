dda388091fee18d9878284cc973445d2b1acfebc
shakil ahmed <shakiltha338@gmail.com>
2024-10-03 01:19:34 +0600

    distributed conditional types

--- chapter04/type_inference.ts
+++ chapter04/type_inference.ts
@@ -97,3 +97,27 @@ let keyAb = getTupleStringAbc<IAb>([1, "test"]);
 console.log(`keyAb = ${keyAb}`);
 let keyAbc = getTupleStringAbc<IAbc>([1, "test", true]);
 console.log(`keyAbc= ${keyAbc}`);
+
+// distributed conditional types
+
+type dateOrNumberOrString<T> = T extends Date
+  ? Date
+  : T extends number
+  ? Date | number
+  : T extends string
+  ? Date | number | string
+  : never;
+
+function compareValues<T extends string | number | Date | boolean>(
+  input: T,
+  compareTo: dateOrNumberOrString<T>
+) {
+  // do comparison
+}
+
+compareValues(new Date(), new Date());
+compareValues(1, new Date());
+compareValues(1, 2);
+compareValues("test", new Date());
+compareValues("test", 1);
+compareValues("test", "test");
