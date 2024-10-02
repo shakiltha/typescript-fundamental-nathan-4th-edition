5d5d089a75fbf814e29280b4e900d1234dd9437c
shakil ahmed <shakiltha338@gmail.com>
2024-10-02 23:31:12 +0600

    conditional type chaining

--- chapter04/type_inference.ts
+++ chapter04/type_inference.ts
@@ -59,3 +59,41 @@ logNumberOrString<number>(1);
 logNumberOrString<string>("test");
 // logNumberOrString<boolean>(true);
 logNumberOrString<boolean>("boolean does not extend number");
+
+// conditional type chaining
+interface IA {
+  a: number;
+}
+interface IAb {
+  a: number;
+  b: string;
+}
+interface IAbc {
+  a: number;
+  b: string;
+  c: boolean;
+}
+
+type abc_ab_a<T> = T extends IAbc
+  ? [number, string, boolean]
+  : T extends IAb
+  ? [number, string]
+  : T extends IA
+  ? [number]
+  : never;
+
+function getTupleStringAbc<T>(tupleValue: abc_ab_a<T>): string {
+  let [...tupleDestructured] = tupleValue;
+  let returnString = "|";
+  for (let value of tupleDestructured) {
+    returnString += `${value}|`;
+  }
+  return returnString;
+}
+
+let keyA = getTupleStringAbc<IA>([1]);
+console.log(`keyA = ${keyA}`);
+let keyAb = getTupleStringAbc<IAb>([1, "test"]);
+console.log(`keyAb = ${keyAb}`);
+let keyAbc = getTupleStringAbc<IAbc>([1, "test", true]);
+console.log(`keyAbc= ${keyAbc}`);
