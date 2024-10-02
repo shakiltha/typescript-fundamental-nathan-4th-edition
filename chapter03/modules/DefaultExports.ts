b6256e555b90f047a1556fddca4b036a36d9f4b4
shakil ahmed <shakiltha338@gmail.com>
2024-10-02 15:43:41 +0600

    default export and nonDefaultExport

---
+++ chapter03/modules/DefaultExports.ts
@@ -0,0 +1,5 @@
+export default function defaultAdd(a: number, b: number) {
+  return a + b;
+}
+
+export class ModuleNonDefaultExport {}
--- chapter03/modules_main.ts
+++ chapter03/modules_main.ts
@@ -1,6 +1,9 @@
 // importing modules
 
 import { Module1 } from "./modules/Module1";
+import { MultipleClass1, MultipleClass2 } from "./modules/MultipleExports";
+import defaultAdd, { ModuleNonDefaultExport } from "./modules/DefaultExports";
+
 let mod1 = new Module1();
 mod1.print();
 
@@ -10,3 +13,11 @@ import { Module1 as MyMod1 } from "./modules/Module1";
 
 let myRenameMod = new MyMod1();
 myRenameMod.print();
+
+let mc1 = new MultipleClass1();
+let mc2 = new MultipleClass2();
+
+let modDefault = defaultAdd(1, 2);
+console.log(modDefault);
+
+let modNonDefault = new ModuleNonDefaultExport();
