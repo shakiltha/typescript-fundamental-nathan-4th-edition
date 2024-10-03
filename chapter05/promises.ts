f7841671975ba1001ef3bfbc83a102352534dbf1
shakil ahmed <shakiltha338@gmail.com>
2024-10-03 22:50:42 +0600

    promise syntax

--- chapter05/promises.ts
+++ chapter05/promises.ts
@@ -21,3 +21,22 @@ fs.readFile("test1.txt", (err, data) => {
     });
   }
 });
+
+// promise syntax
+
+fs.promises
+  .readFile("./test1.txt")
+  .then((value) => {
+    console.log(`ps test1.txt read : ${value}`);
+    return fs.promises.readFile("./test2.txt");
+  })
+  .then((value) => {
+    console.log(`ps test2.txt read : ${value}`);
+    return fs.promises.readFile("./test3.txt");
+  })
+  .then((value) => {
+    console.log(`ps test3.txt read : ${value}`);
+  })
+  .catch((error) => {
+    console.log(`an error occurred : ${error}`);
+  });
