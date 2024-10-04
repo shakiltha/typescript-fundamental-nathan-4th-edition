a7bbeba93cc7c2b1d79cb05c0d0d92f087846e32
shakil ahmed <shakiltha338@gmail.com>
2024-10-04 21:47:59 +0600

    await errors

--- chapter05/async-and-await.ts
+++ chapter05/async-and-await.ts
@@ -15,3 +15,27 @@ async function callDelayedPromise() {
 }
 
 callDelayedPromise();
+
+// await errors
+
+function errorPromise(): Promise<string> {
+  return new Promise<string>(
+    (resolve: (result: string) => void, reject: (error: string) => void) => {
+      setTimeout(() => {
+        console.log(`2. calling reject()`);
+        reject("promise rejected");
+      }, 2000);
+    }
+  );
+}
+
+async function callErrorPromise() {
+  try {
+    console.log(`1. calling errorPromise()`);
+    await errorPromise();
+  } catch (error) {
+    console.log(`3. await threw : ${error}`);
+  }
+}
+
+callErrorPromise();
