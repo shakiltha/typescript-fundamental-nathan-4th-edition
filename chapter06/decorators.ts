5a00713f6e0f9410d669b4f1f12d84d49dfa5ad6
shakil ahmed <shakiltha338@gmail.com>
2024-10-05 18:59:07 +0600

    using method decorators

--- chapter06/decorators.ts
+++ chapter06/decorators.ts
@@ -104,3 +104,23 @@ class StaticClassWithPropertyDec {
   @propertyDec
   static staticProperty: string;
 }
+
+// method decorators
+function methodDec(
+  target: any,
+  methodName: string,
+  descriptor?: PropertyDescriptor
+) {
+  console.log(`target: ${target}`);
+  console.log(`methodName : ${methodName}`);
+  console.log(`descriptor : ${JSON.stringify(descriptor)}`);
+
+  console.log(`target[methodName] : ${target[methodName]}`);
+}
+
+class ClassWithMethodDec {
+  @methodDec
+  print(output: string) {
+    console.log(`ClassWithMethodDec.print` + `(${output}) called.`);
+  }
+}
