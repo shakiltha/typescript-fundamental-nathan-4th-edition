102bdcf7e292449cdaa84c1bcedfa5465cefbdaa
shakil ahmed <shakiltha338@gmail.com>
2024-10-05 18:28:13 +0600

    class decorators

--- chapter06/decorators.ts
+++ chapter06/decorators.ts
@@ -63,3 +63,20 @@ function decoratorFactories(name: string) {
 
 @decoratorFactories("test name")
 class ClassWithDecoratorFactory {}
+
+// class decorators
+
+function classConstructorDec(constructor: Function) {
+  console.log(`constructor : ${constructor}`);
+  constructor.prototype.testProperty = "testProperty_value";
+}
+
+@classConstructorDec
+class ClassWithConstructor {
+  constructor(id: number) {}
+}
+
+let classInstance = new ClassWithConstructor(1);
+console.log(
+  `classInstance.testProperty = ${(<any>classInstance).testProperty}`
+);
