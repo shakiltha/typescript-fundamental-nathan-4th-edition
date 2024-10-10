f73cdd4a769c186e61364899cd3ff10a0180f5b7
shakil ahmed <shakiltha338@gmail.com>
2024-10-10 16:59:41 +0600

    matcher in jest

---
+++ chapter10/src/matchers.spec.ts
@@ -0,0 +1,38 @@
+function throwsError() {
+  throw new Error("this is an error");
+}
+
+describe("test matchers", () => {
+  it("should match with toBe", () => {
+    expect(1).toBe(1);
+  });
+
+  it("should match with toBe using assignment", () => {
+    let objA = { id: 1 };
+    let objB = objA;
+    expect(objA).toBe(objB);
+  });
+
+  it("should match with toEqual", () => {
+    let objA = { id: 1 };
+    let objB = { id: 1 };
+    expect(objA).toEqual(objB);
+  });
+
+  it("should contain a value", () => {
+    expect("abcde").toContain("cde");
+  });
+
+  it("should contain an array item", () => {
+    let objArray = [{ id: 1 }, { id: 2 }];
+    expect(objArray).toContainEqual({ id: 2 });
+  });
+
+  it("should not contain a value", () => {
+    expect("ab1c2d3e").not.toContain("123");
+  });
+
+  it("should throw an error", () => {
+    expect(() => throwsError()).toThrow(new Error("this is an error"));
+  });
+});
