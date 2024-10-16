274a4cb4c2c97129eec988af4820ea9d150f6439
shakil ahmed <shakiltha338@gmail.com>
2024-10-17 01:24:24 +0600

    added BroadcastService in app.module so that any component have access to this service

--- chapter11/angular-app/src/app/app.module.ts
+++ chapter11/angular-app/src/app/app.module.ts
@@ -4,14 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { SharedModule } from './shared/shared.module';
-import { UserDetailsComponent } from './user-details/user-details.component';
+import { BroadcastService } from './services/broadcast.service';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserDetailsComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [AppRoutingModule, SharedModule],
-  providers: [provideAnimationsAsync()],
+  providers: [BroadcastService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
