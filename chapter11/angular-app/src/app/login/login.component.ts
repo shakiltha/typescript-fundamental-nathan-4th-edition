bdae8bdc89d299447fd785997a391c1479f5516c
shakil ahmed <shakiltha338@gmail.com>
2024-10-18 16:34:59 +0600

    added functionality to LoginComponent, implemented OnInit

--- chapter11/angular-app/src/app/login/login.component.ts
+++ chapter11/angular-app/src/app/login/login.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { BroadcastService } from '../services/broadcast.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup | null = null;
+  constructor(
+    private formBuilder: FormBuilder,
+    private broadcastService: BroadcastService
+  ) {}
+  ngOnInit(): void {
+    console.log(`buildForm()`);
+    this.buildForm();
+  }
+  buildForm() {
+    let form = {
+      username: new FormControl({}, Validators.required),
+      password: new FormControl({}, Validators.required),
+    };
+    let formState = {
+      username: {
+        value: '',
+        disabled: false,
+      },
+      password: {
+        value: '',
+        disabled: false,
+      },
+    };
+    this.loginForm = this.formBuilder.group(form);
+    this.loginForm.reset(formState);
+  }
+  isFormValid() {
+    return this.loginForm?.valid;
+  }
 }
